83f9eec3f350bebcdbbae82af68759acf837760d
mflops <madhurchawla10f@gmail.com>
2025-07-08 00:36:24 +0530

    chore: add static profile image

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,8 @@
 export default function Profile() {
     return (
         <div className="flex flex-row items-center gap-2 h-full pl-2 pt-1 pb-1 pr-2 cursor-pointer hover:rounded-md hover:bg-[#EEEEEE]" onClick={() => (console.log("Profile clicked!"))}>
-            <div className="bg-neutral-200 h-7 w-7 rounded-full">
-            </div>
+            <img className="bg-neutral-200 h-7 w-7 rounded-full" src="/pfp.png">
+            </img>
             <div className="flex flex-col h-6 justify-center">
                 <p className="text-xs m-0 leading-none font-regular w-24 text-[#121212]">John Doe</p>
                 <p className="text-[#757575] text-xs m-0 leading-none font-regular w-24">john.doe...</p>
